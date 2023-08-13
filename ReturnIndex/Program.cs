05b7e5c3c86073551d6976d72c288adf8428d66c
acer <acer@ASUR>
2023-08-13 21:47:23 +0530

    Add project files.

---
+++ ReturnIndex/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 25, 5, 30, 15 };
+            int target = 30;
+            int foundIndex = -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == target)
+                {
+                    foundIndex = i;
+                    break;
+                }
+            }
+
+            if (foundIndex != -1)
+            {
+                Console.WriteLine("Target value found at index: " + foundIndex);
+            }
+            else
+            {
+                Console.WriteLine("Target value not found in the array.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
